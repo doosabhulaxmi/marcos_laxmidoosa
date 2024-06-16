ca2f94197f51476eb7dcc3b62b12965702ec99c0
doosabhulaxmi <bhulaxmi1999@gmail.com>
2024-06-16 16:03:59 +0530

    my first git commithttps://github.com/doosabhulaxmi/marcos_laxmidoosa.git

---
+++ js/script.js
@@ -0,0 +1,98 @@
+// variables
+var name = "Bhulaxmi";
+var surname = "Doosa";
+var age = 22;
+var school = "PT High school";
+var number = 56768;
+var Friend = "Bomma";
+var mother = "laxmi";
+var Address = "balaji nagar bandari counpound";
+
+
+
+
+
+let ab = 10;
+let name1 = 10;
+
+let isFriend = false;
+
+
+
+//  Data types 
+/**
+ * 1. number - 10, 11, 122
+ * 2. string - "", '',
+ * 3. boolean - true or false,
+ * 4. Objects - {}.
+ * 5. BigInt
+ */
+console.log(name);
+console.log(surname);
+console.log(age);
+console.log(isFriend);
+console.log(number)
+console.log(Friend)
+console.log(mother)
+console.log(Address)
+
+
+console.log('My name is ' + name + " and surname is " + surname)
+console.log('my mobile number is '+ number + " she is my "+Friend)
+console.log("my mother ")
+console.log("my name is " + name + " and my add is " +Address + " i am completed my study " + school)
+
+
+var a = 10;
+var b = 20;
+var c = 40;
+var d = 90;
+console.log("a+b+c/b",a+b+c/b);
+console.log("a-b-c", a-b-c);
+console.log("a*b",a*b);
+console.log("a/b",a/b);
+console.log("a%b",a%b);
+
+
+
+var blossom = "Jwel Brand";
+var GelColours = "Glazing Gel";
+var foodcolour = "Joker Brand";
+var allbrands = "Blossom";
+
+console.log(blossom)
+console.log(GelColours)
+console.log(foodcolour)
+console.log(allbrands)
+
+
+console.log("Gel colours, "+ GelColours +" this are " + foodcolour+ " and this brand included in "+allbrands)
+
+
+var name2 = "Bhulaxmi";
+var surname2 = "Doosa";
+var office = "Kalher";
+var salary = 10000;
+var frds = 7;
+var company = "Raunak Enterprises"
+
+console.log(name2)
+console.log(surname2)
+console.log(office)
+console.log(salary)
+console.log(frds)
+console.log(company)
+
+console.log("Hello my namme is " + name2  + " and surname is " + surname2 + " i am worked at " + company + " it is located in " + office)
+
+console.log(" hello " + GelColours + " name " + surname2)
+Boolean = true, false
+test = 10 > 20;
+console.log(test);
+
+let tv = "30";
+ tv = "40";
+ tv = "50";
+
+ console.log(tv)
+
